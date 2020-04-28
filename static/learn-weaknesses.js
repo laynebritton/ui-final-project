94650767f0939e50c158008450470e0c52bd55be
laynebritton <laynebritton1998@gmail.com>
2020-04-28 14:40:18 -0600

    Implemented active status for learning pagination

---
+++ static/learn-weaknesses.js
@@ -0,0 +1,4 @@
+
+$(document).ready(function () {
+    set_active_learn_tab_to_active("#weaknesses-tab")
+})
--- static/learn.js
+++ static/learn.js
@@ -6,6 +6,16 @@ function set_quiz_tab_to_active(tab) {
     $(tab).addClass("active")
 }
 
+function set_active_learn_tab_to_active(tab) {
+    $("#intro-tab").removeClass("active")
+    $("#strengths-tab").removeClass("active")
+    $("#weaknesses-tab").removeClass("active")
+    $("#types1-tab").removeClass("active")
+    $("#types2-tab").removeClass("active")
+    $("#multiple-types-tab-tab").removeClass("active")
+
+    $(tab).addClass("active")
+}
 
 $(document).ready(function () {
 
