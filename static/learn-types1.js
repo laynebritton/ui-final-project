b03b4332c1434b3000ab6e49cb86fbabfff0db02
laynebritton <laynebritton1998@gmail.com>
2020-04-28 23:00:02 -0600

    Type explanations auto collapse other types when click

--- static/learn-types1.js
+++ static/learn-types1.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
     set_active_learn_tab_to_active("#types1-tab")
     set_next_page_link("/learn/types2")
     set_previous_page_link("/learn/weaknesses")
+
+    $("#fire-collapse").click(function() { 
+        $('#water-type-chart').removeClass('show')
+        $('#grass-type-chart').removeClass('show')
+
+     })
+
+    $("#water-collapse").click(function() { 
+        $('#fire-type-chart').removeClass('show')
+        $('#grass-type-chart').removeClass('show')
+     })
+
+     $("#grass-collapse").click(function() { 
+        $('#fire-type-chart').removeClass('show')
+        $('#water-type-chart').removeClass('show')
+
+     })
 })
