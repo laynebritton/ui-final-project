5b4fc69fd7c345c0fd56926a9902a5ae7661f5f7
laynebritton <laynebritton1998@gmail.com>
2020-04-29 00:19:26 -0600

    There is a functional home page

--- static/home.js
+++ static/home.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function () {
     update_active_tab("#home-tab")
-
+    $("#learn-button").click(function () {
+        window.location.href = '/learn'        
+    })
+    $("#quiz-button").click(function () {
+        window.location.href = '/quiz'        
+    })
 })
