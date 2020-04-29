c7cce5ff45c350b319b9e58eb502a3ec35812e1c
laynebritton <laynebritton1998@gmail.com>
2020-04-28 23:44:13 -0600

    types2 page populated

--- static/learn-types2.js
+++ static/learn-types2.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
     set_active_learn_tab_to_active("#types2-tab")
     set_next_page_link("/learn/multiple_types")
     set_previous_page_link("/learn/types1")
+
+    $("#flying-collapse").click(function() { 
+        $('#rock-type-chart').removeClass('show')
+        $('#fighting-type-chart').removeClass('show')
+
+     })
+
+    $("#rock-collapse").click(function() { 
+        $('#flying-type-chart').removeClass('show')
+        $('#fighting-type-chart').removeClass('show')
+     })
+
+     $("#fighting-collapse").click(function() { 
+        $('#flying-type-chart').removeClass('show')
+        $('#rock-type-chart').removeClass('show')
+
+     })
 })
